46d5e0b78449f217ca1410b0db33a26f01054019
anton <antonkuzmn1@gmail.com>
2025-02-06 01:36:38 +0700

    generate and print matrix

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+const int SIZE = 5;
+
+void fillMatrix(int **matrix) {
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            matrix[i][j] = rand() % 100;
+        }
+    }
+}
+
+void printMatrix(int **matrix) {
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main() {
-    std::cout << "Hello, CMake!" << std::endl;
+    srand(time(0));
+
+    int **matrix = new int *[SIZE];
+    for (int i = 0; i < SIZE; ++i) {
+        matrix[i] = new int[SIZE];
+    }
+
+    printMatrix(matrix);
+
+    fillMatrix(matrix);
+
+    printMatrix(matrix);
+
+    // std::cout << "Hello, CMake!" << std::endl;
     return 0;
 }
