b4a59f3f775178359fc4bcbe62403f39959b20f4
anton <antonkuzmn1@gmail.com>
2025-02-06 02:11:27 +0700

    print diagonals

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <random>
 #include <vector>
 
 using namespace std;
 
 const int SIZE = 5;
 
+int generateRandomNumber(int min, int max) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(min, max);
+    return dis(gen);
+}
+
 void fillMatrix(int **matrix) {
     for (int i = 0; i < SIZE; ++i) {
         for (int j = 0; j < SIZE; ++j) {
-            matrix[i][j] = rand() % 100;
+            matrix[i][j] = generateRandomNumber(10, 99);
         }
     }
 }
@@ -24,20 +30,76 @@ void printMatrix(int **matrix) {
     }
 }
 
-int main() {
-    srand(time(0));
+void printRightDiagonals(int** matrix) {
+    vector<int> result;
+    for (int start = 0; start < SIZE; ++start) {
+        int row = 0, col = start;
+        while (row < SIZE && col < SIZE) {
+            result.push_back(matrix[row][col]);
+            ++row;
+            ++col;
+        }
+    }
+    for (int start = 1; start < SIZE; ++start) {
+        int row = start, col = 0;
+        while (row < SIZE && col < SIZE) {
+            result.push_back(matrix[row][col]);
+            ++row;
+            ++col;
+        }
+    }
 
+    for (const int i : result) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+void printLeftDiagonals(int** matrix) {
+    vector<int> result;
+    for (int start = 0; start < SIZE; ++start) {
+        int row = 0, col = start;
+        while (row < SIZE && col >= 0) {
+            result.push_back(matrix[row][col]);
+            ++row;
+            --col;
+        }
+    }
+    for (int start = 1; start < SIZE; ++start) {
+        int row = start, col = SIZE - 1;
+        while (row < SIZE && col >= 0) {
+            result.push_back(matrix[row][col]);
+            ++row;
+            --col;
+        }
+    }
+
+    for (const int i : result) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main() {
     int **matrix = new int *[SIZE];
     for (int i = 0; i < SIZE; ++i) {
         matrix[i] = new int[SIZE];
     }
 
-    printMatrix(matrix);
-
     fillMatrix(matrix);
 
+    std::cout << "\nматрица" << endl;
     printMatrix(matrix);
 
-    // std::cout << "Hello, CMake!" << std::endl;
+    std::cout << "\na) по правым диагоналям, начиная с правого верхнего элемента" << endl;
+    printRightDiagonals(matrix);
+
+    std::cout << "\nb) по левым диагоналям, начиная с левого верхнего элемента" << endl;
+    printLeftDiagonals(matrix);
+
+    for (int i = 0; i < SIZE; ++i) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
     return 0;
 }
